70f5f23bb046d44fefe47af788217ef033b54ab1
Pedro Marzano <musicomarzano@gmail.com>
2020-08-21 11:09:03 -0300

    up ex click button counter csharp

---
+++ aula002-app-avaliacao-clientes/Avaliacao-clientes/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Avaliacao_clientes
+{
+    public partial class Form1 : Form
+    {
+        int counterS = 0;
+        int counterN = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            counterS++;
+            textBox1.Text = counterS.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            counterN++;
+            textBox2.Text = counterN.ToString();
+        }
+    }
+}
